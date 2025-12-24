225a7e5432ee6ea823bf367ec80da1a8434cd4b3
mawwyncha <maulida.dc@gmail.com>
2025-12-24 16:02:05 +0700

    update config js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports ={
+    purge: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+    darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        forest: {
+                            50: '#f0f7f4',
+                            900: '#05140d',
+                            950: '#020a06',
+                        }
+                    }
+                }
+            }
+    plugins: [],
+};
